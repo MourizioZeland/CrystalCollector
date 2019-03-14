518d53324d5cfadb75ff519d88f917b98648909f
Mourizio Zeland <zelandmourizio@gmail.com>
2019-03-14 00:54:25 -0700

    Update

--- games/game.js
+++ games/game.js
@@ -3,7 +3,9 @@ $( document ).ready(function() {
 //empty value array
 var crystalValue = [];
 var counter = 0;
-var secondNumber= "";
+var secondNumber= crystalValue.concat(counter);
+var losses = 0;
+var wins = 0;
 // this will generate random number for computer
 
 var computerPick = Math.floor(Math.random() * 101) + 19;
@@ -37,29 +39,42 @@ $("#crystals").append(imagePlacer);
 console.log(imagePlacer);
 
 }
-$("#crystals").on("click", function() {
+$(".crystals-image").on("click", function() {
     var crystalValue = ($(this).attr("data-crystalvalue"));
+    $("#win2").text(crystalValue);
+    console.log(crystalValue);
+
     crystalValue = parseInt(crystalValue);
     counter += crystalValue;
 
-    
-   if (randomNumber !== computerPick){
-    $("#win2").text(randomNumber);
+    if(crystalValue !== computerPick){
+      $("#win2").text(crystalValue);
+      }
+
+   
+   if(crystalValue !== computerPick){
+      $("#win2").text(counter);
+     
    }
-   if(randomNumber < computerPick){
-     $("#win2").text(randomNumber + secondNumber)
+   function reset(){
+    var computerPick = Math.floor(Math.random() * 101) + 19;
+    $("#numberplacer").text(computerPick); 
+    counter = 0;
+
    }
 
-    if(randomNumber < computerPick){
-    $("#win2").text(randomNumber);
-    }
-    else if (counter === computerPick) {
-      alert("You win!");
+    
+    if (counter === computerPick) {
+      alert("You win!"); reset(); wins++;
+      $("#winner").text("wins:" + wins);
+
     }
 
     else if (counter >= computerPick) {
-      alert("You lose!!");
+      alert("You lose!!"); reset(); losses++;
+      $("#winner").text("losses: " + losses);
     }
+    
 
 })
 
